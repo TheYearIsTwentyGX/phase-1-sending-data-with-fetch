ceb30b286891262e2a9e7da2077c21837798ffb3
Dylan Hays <twentygx@outlook.com>
2022-07-26 20:25:03 -0500

    commit tree cf9615eeecdd62ce1743f2d3403ef96051b1afa3

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({"name": name, "email": email}),
+    }).then(resp => resp.json())
+    .then(user => {
+        document.querySelector('script').textContent = user.id;
+    })
+    .catch(error => document.querySelector('script').textContent = error.message);
+}
